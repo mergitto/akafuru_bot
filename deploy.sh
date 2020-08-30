ced18bd755b272c1e42ce9948c896c6ecb9ea7b9
github.username <github.email>
2020-08-30 13:29:45 +0900

    Fix allUsers は gcp console から直接設定するようにした

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,4 @@ gcloud functions deploy "$FUNCTION_NAME" \
 --runtime go113 \
 --region asia-northeast1 \
 --trigger-http \
---allow-unauthenticated \
 --set-env-vars VERIFICATION_TOKEN="$VERIFICATION_TOKEN",API_KEY="$API_KEY"
