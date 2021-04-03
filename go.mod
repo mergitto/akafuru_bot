a8c4656b05d5ee0cd810b3c813f5ea4297918986
mergitto <merugitosu@gmail.com>
2021-04-03 10:51:18 +0900

    fix 月指定で歩数を取得できるようにした

--- go.mod
+++ go.mod
@@ -1,15 +1,13 @@
 module github.com/mergitto/akafuru_bot
 
-go 1.14
+go 1.16
 
 require (
 	github.com/dustin/go-humanize v1.0.0
 	github.com/go-resty/resty/v2 v2.3.0
 	github.com/gorilla/websocket v1.4.2 // indirect
-	github.com/kr/pretty v0.1.0 // indirect
 	github.com/nlopes/slack v0.6.0
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/stretchr/testify v1.4.0 // indirect
 	golang.org/x/net v0.0.0-20200822124328-c89045814202 // indirect
-	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 )
--- handler/steps.go
+++ handler/steps.go
@@ -21,9 +21,8 @@ type stepsEachMonth struct {
 	AllSteps int     `json:"all_steps"`
 }
 
-// TODO: 指定した月の歩数が取れるようにしたい
 func Steps(w http.ResponseWriter, s slack.SlashCommand) {
-	msg, err := toMessage(getSteps(w))
+	msg, err := toMessage(getSteps(w, s.Text), s.Text)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
@@ -36,8 +35,14 @@ func Steps(w http.ResponseWriter, s slack.SlashCommand) {
 	writeBody(w, b)
 }
 
-func toMessage(steps *stepsEachMonth) (*slack.Msg, error) {
-	msg := "今月の歩数をお知らせするニャー！\n"
+func toMessage(steps *stepsEachMonth, yearMonth string) (*slack.Msg, error) {
+	var msg string
+	if yearMonth != "" {
+		msg = yearMonth
+	} else {
+		msg = "今月"
+	}
+	msg += "の歩数をお知らせするニャー！\n"
 	for _, step := range steps.Steps {
 		t, err := time.Parse("2006-01-02", step.DateMonth)
 		if err != nil {
@@ -46,22 +51,27 @@ func toMessage(steps *stepsEachMonth) (*slack.Msg, error) {
 		month := fmt.Sprintf("%d年%d月%d日", t.Year(), t.Month(), t.Day())
 		stepComma := humanize.Comma(int64(step.Value))
 		switch {
-		case step.Value < 1000:
+		case step.Value < 3000:
 			msg += fmt.Sprintf("%s: %s歩 (デブへの道へと続いてるニャ…)\n", month, stepComma)
-		case step.Value > 15000:
-			msg += fmt.Sprintf("%s: %s歩 (そんなに運動してどうするニャ!!)\n", month, stepComma)
 		case step.Value > 10000:
+			msg += fmt.Sprintf("%s: %s歩 (そんなに運動してどうするニャ!!)\n", month, stepComma)
+		case step.Value > 8000:
 			msg += fmt.Sprintf("%s: %s歩 (よく運動したニャ!)\n", month, stepComma)
 		default:
 			msg += fmt.Sprintf("%s: %s歩\n", month, stepComma)
 		}
 	}
 	msg += "\n"
-	msg += fmt.Sprintf("今月の累計歩数は %s歩 ですニャー :cat2:", humanize.Comma(int64(steps.AllSteps)))
+	if yearMonth != "" {
+		msg += yearMonth
+	} else  {
+		msg += "今月"
+	}
+	msg += fmt.Sprintf("の累計歩数は %s歩 ですニャー :cat2:", humanize.Comma(int64(steps.AllSteps)))
 	return &slack.Msg{ResponseType: "in_channel", Text: msg}, nil
 }
 
-func getSteps(w http.ResponseWriter) *stepsEachMonth {
+func getSteps(w http.ResponseWriter, req string) *stepsEachMonth {
 	api := os.Getenv("BLOG_ACTIVITY_API")
 	if api == "" {
 		w.WriteHeader(http.StatusBadRequest)
@@ -71,7 +81,15 @@ func getSteps(w http.ResponseWriter) *stepsEachMonth {
 
 	sts := &stepsEachMonth{}
 	client := resty.New()
-	resp, err := client.R().EnableTrace().SetResult(sts).Get(fmt.Sprintf("%s/meru/steps/each_month", api))
+	var resp *resty.Response
+	var err error
+	var path string
+	if req != "" {
+		path = fmt.Sprintf("%s/meru/steps/each_month/%s", api, req)
+	} else {
+		path = fmt.Sprintf("%s/meru/steps/each_month", api)
+	}
+	resp, err = client.R().EnableTrace().SetResult(sts).Get(path)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		writeBody(w, []byte(err.Error()))
